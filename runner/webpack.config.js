7f4dfed668f09a2ba77a96b9568c646b47bba64d
coderitual <skowronkow@gmail.com>
2021-08-23 21:46:14 +0200

    Add style guide app

---
+++ runner/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.jsx',
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
