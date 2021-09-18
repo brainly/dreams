3399d99b315492f89e9045efd8461c6a2ac92a2a
coderitual <skowronkow@gmail.com>
2021-09-18 13:19:23 +0200

    Change default scale mode in bg image

--- packages/core/src/dom/scene-from-element.ts
+++ packages/core/src/dom/scene-from-element.ts
@@ -111,7 +111,7 @@ export async function createSceneNodeFromElement(element) {
       {
         type: 'IMAGE',
         blendMode: 'NORMAL',
-        scaleMode: 'FILL',
+        scaleMode: 'FIT',
         imageHash: imageBase64 as string,
       },
     ];
