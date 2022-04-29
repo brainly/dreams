7503484c46fb1c8029390712ae676a586f2d1b7b
coderitual <skowronkow@gmail.com>
2022-04-29 20:51:01 +0200

    Rewrite apply overrides

--- packages/core/src/helpers/overrides.ts
+++ packages/core/src/helpers/overrides.ts
@@ -1,30 +1,26 @@
 import { SceneNode } from '../nodes/scene';
-// TODO: this is a mess, need to clean up
+
 export function applyOverrides(dest: SceneNode, source: SceneNode) {
   for (const key in source) {
-    if (key in dest) {
-      if (
-        key === 'type' ||
-        key === 'id' ||
-        key === 'name' ||
-        typeof source[key] === 'function'
-      ) {
-        continue;
-      }
+    if (
+      key === 'type' ||
+      key === 'id' ||
+      key === 'name' ||
+      key === 'parent' ||
+      typeof source[key] === 'function'
+    ) {
+      continue;
+    }
 
-      // apply overrides within children nodes
-      if (key === 'children') {
-        dest[key] = dest[key].map((child, index) => {
-          if (source[key][index]) {
-            return applyOverrides(child, source[key][index]);
-          } else {
-            return child;
-          }
-        });
-      } else {
-        dest[key] = source[key];
-      }
+    // apply overrides within children nodes
+    if (key === 'children') {
+      source[key].forEach((child, index) => {
+        applyOverrides(child, source[key][index]);
+      });
+    } else {
+      dest[key] = source[key];
     }
   }
+
   return dest;
 }
